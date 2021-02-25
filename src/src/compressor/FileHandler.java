575ad696c9b353f8fd2fea7b77d822f0d9d7eb08
Daniel McCarthy <dan.willy.mccarthy@gmail.com>
2021-02-25 01:55:30 -0600

    Add helper class to handle File IO

---
+++ src/src/compressor/FileHandler.java
@@ -0,0 +1,34 @@
+package compressor;
+
+import java.io.*;
+import java.util.List;
+
+public class FileHandler {
+	public static byte[] openFile(String filePath) {
+		try {
+			FileInputStream inputFile = new FileInputStream(filePath);
+			byte[] fileData = inputFile.readAllBytes();
+			inputFile.close();
+			return fileData;	
+		} catch (IOException e) {
+			System.out.println(String.format("Failed to find or read file at: \"%s\". Exception: %s", filePath, e.getMessage()));
+		}
+		return null;
+
+	}
+	
+	public static void saveFile(String filePath, List<Byte> fileData) {
+		try {
+			FileOutputStream outputFile = new FileOutputStream(filePath);
+			byte[] fileDataArray = new byte[fileData.size()];
+			
+			for (int i = 0; i < fileDataArray.length; i++)
+				fileDataArray[i] = fileData.get(i);
+			
+			outputFile.write(fileDataArray);
+			outputFile.close();
+		} catch (IOException e) {
+			System.out.println(String.format("Failed to save file to: \"%s\". Exception: %s", filePath, e.getMessage()));
+		}
+	}
+}
