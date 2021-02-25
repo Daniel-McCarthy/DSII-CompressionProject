d0dda6889ddc775d296dbb37aacbd93d5d452b00
Daniel McCarthy <dan.willy.mccarthy@gmail.com>
2021-02-25 02:00:20 -0600

    Include demonstration of FileHandler use

--- src/src/compressor/FileCompressor.java
+++ src/src/compressor/FileCompressor.java
@@ -1,10 +1,23 @@
 package compressor;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import compressor.FileHandler;
+
 public class FileCompressor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("test");
+//		Input Example:
+//		byte[] fileData = FileHandler.openFile("../samples/peterpiper.txt");
+//		for (byte byteValue : fileData)
+//			System.out.print(String.format("%c ", byteValue));
+//		Output Example:
+//		List<Byte> fileDataAsList = new ArrayList<Byte>();
+//		for (Byte byteValue : fileData)
+//			fileDataAsList.add(byteValue);
+//		FileHandler.saveFile("../samples/peterpiper-output.txt", fileDataAsList);
 	}
 
 }
