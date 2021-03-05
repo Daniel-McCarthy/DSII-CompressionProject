5c5fa4178ccd81acb6d59303e50929891b4be6ae
Daniel McCarthy <dan.willy.mccarthy@gmail.com>
2021-03-04 20:45:26 -0600

    Added support for giving input as a file and saving it as a compressed file
    Previously the main input method was entering a string. Now we can load in a file by entering the filename instead.
    
    This will open the file, compress it, display information on the compression process, and then save the output as a compressed file.

--- src/src/compressor/FileCompressor.java
+++ src/src/compressor/FileCompressor.java
@@ -1,5 +1,6 @@
 package compressor;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -25,12 +26,29 @@ public class FileCompressor {
 		 System.out.println("Welcome to our compression program."
 				 + "\n\t(1. Text input."
 				 + "\n\t(2. File input."
-				 + "\n\n Please enter a number to select your input method:");
+				 + "\n\nPlease enter a number to select your input method:");
 		 
 		 String input = scanner.nextLine();
 		 // Default to text input if input is not explicitly for File input.
-		 if (input == "2" || input.contains("File") || input.contains("file")) {
+		 if (input.equals("2") || input.contains("File") || input.contains("file")) {
 			// Ask for a filename to open.
+			boolean retry = false;
+			String fileName = "";
+			while (fileName.equals("") || retry) {
+				System.out.println("\n\nPlease enter the name of the file you wish to open:");
+				fileName = scanner.nextLine();
+				File file = new File(fileName);
+				
+				if (file.exists()) {
+					System.out.println("\n"); // Extra line to add room before next output.
+					compressFileAndPrintResult(file);
+					retry = false;
+					
+				} else {
+					retry = true;
+					System.out.println("\nFile not found. Please try again.\n");
+				}
+			}
 			 
 		 } else {
 			 // Ask for text input to compress.
@@ -39,6 +57,37 @@ public class FileCompressor {
 			 compressStringAndPrintResult(textToCompress);
 		 }
 		 scanner.close();
+	 }
+	 
+	 static void compressFileAndPrintResult(File fileToCompress) {
+		 String fileName = fileToCompress.getName();
+		 String fileNameWithoutExtension = fileToCompress.getName().substring(0, fileName.lastIndexOf('.'));
+		 String fileExtension = fileName.substring(fileName.lastIndexOf('.'));
+		 
+		 byte[] fileData = FileHandler.openFile(fileToCompress);
+		 String textToCompress = "";
+		 for (byte character : fileData) {
+			 textToCompress += (char)character;
+		 }
+		 
+		 String compressedData = CompressionAlgorithm.compress(textToCompress);
+		 String compressedDataAsText = CompressionAlgorithm.numberStringToText(compressedData);
+		 String decompressedText = CompressionAlgorithm.decompress(compressedData);
+		 String fileNameForCompressedData = String.format("%s.c%s", fileNameWithoutExtension, fileExtension);
+		 
+		 // Print input/output text, their sizes, and the percentage difference between the two string sizes.
+		 System.out.println(String.format("\n\nInput Text:\n\n'%s'", textToCompress));
+		 System.out.println(String.format("Input Size: %d", textToCompress.length()));
+		 System.out.println(String.format("\n\nCompressed Text Output:\n\n'%s'", compressedDataAsText));
+		 System.out.println(String.format("Compressed Text Size: %d", compressedDataAsText.length()));
+		 
+		 float inputLength = textToCompress.length();
+		 float compressedLength = compressedDataAsText.length();
+		 float differencePercentage = (Math.abs(inputLength - compressedLength) / ((inputLength + compressedLength) / 2)) * 100;
+		 System.out.println(String.format("\n\nPercentage size difference: %s%.2f%%", inputLength > compressedLength ? '-' : '+', differencePercentage));
+	 
+		 FileHandler.saveFile(fileNameWithoutExtension + ".c" + fileExtension, compressedDataAsText);
+		 System.out.println("Wrote compressed file to: " + fileNameForCompressedData);
 	 }
 	 
 	 static void compressStringAndPrintResult(String textToCompress) {
