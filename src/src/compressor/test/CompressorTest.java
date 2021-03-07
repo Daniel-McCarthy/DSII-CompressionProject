12ddbff3583c52ea51c937086778b45a68f989e1
Daniel McCarthy <dan.willy.mccarthy@gmail.com>
2021-03-06 19:51:17 -0600

    Enhance static data test - No repetition validation, fixes
    - Renamed test to mention that it uses static data, this is just one of our methods of testing
    - Cleanup and fixes for test.
    - Add validation to ensure that repeated words in our example only show up a max of 1 time.

--- src/src/compressor/test/CompressorTest.java
+++ src/src/compressor/test/CompressorTest.java
@@ -29,15 +29,25 @@ class CompressorTest {
 	}
 
 	@Test
-	void compressionDecompressionTest() {
-		String inputText = "How much would could a woodchuck chuck if a woodchuck could chuck wood?";
-		String compressedText = algorithm.compress(inputText);
+	void compressionDecompressionTest_StaticData() {
+		String inputText = "How much wood could a woodchuck chuck if a woodchuck could chuck wood?";
+		String[] repeatedWords = new String[] { "wood", "could", "chuck", "a" };
 		
-		Assert.assertFalse(String.format("Compression of string failed to result in any change. Both input and compressed string both were: '%s'.", inputText), inputText.equals(compressedText));
-		Assert.assertTrue(String.format("Compressed text failed to be smaller than input text. Input Length: %d, Compressed Length: %d.", inputText.length(), compressedText.length()), compressedText.length() < inputText.length());
+		String compressedString = algorithm.compress(inputText);
+		String compressedStringAsText = algorithm.numberStringToText(compressedString);
+		String decompressedText = algorithm.numberStringToText(algorithm.decompress(compressedString));
 		
-		String decompressedText = algorithm.decompress(compressedText);
-		Assert.assertTrue(String.format("Decompressed string failed to be the same as input text.\n\tExpected: %s\n\tGot:%s",  inputText, compressedText), inputText.contentEquals(decompressedText));
+		// Validate that compressed data is both different than input, and smaller.
+		Assert.assertFalse(String.format("Compression of string failed to result in any change. Both input and compressed string both were: '%s'.", inputText), inputText.equals(compressedStringAsText));
+		Assert.assertTrue(String.format("Compressed text failed to be smaller than input text. Input Length: %d, Compressed Length: %d.", inputText.length(), compressedStringAsText.length()), compressedStringAsText.length() < inputText.length());
+		
+		// De-compressed data must be exactly identical to input data.
+		Assert.assertTrue(String.format("Decompressed string failed to be the same as input text.\n\tExpected: %s\n\tGot:%s",  inputText, decompressedText), inputText.contentEquals(decompressedText));
+	
+		// Validate that our sentence repetitions do not show up more than once.
+		System.out.println(decompressedText);
+		for (String word : repeatedWords) {
+			Assert.assertEquals(compressedString.indexOf(word), compressedString.lastIndexOf(word));
+		}
 	}
-
 }
